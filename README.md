7d4b7d72b57ae25e2bee62380a704382c4c6a1a7
Eduardo Barbosa <ebdotcontact@gmail.com>
2023-03-31 10:58:12 -0300

    project commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Node Typescript Structure
+A node typescript structure example.
+
+# Technologies
+* nodejs
+* typescript
+* dotenv
+* tsx
+* tsup
+* vitest
---
+++ src/tests/example.spec.ts
@@ -0,0 +1,7 @@
+import {describe, test, expect} from 'vitest';
+
+describe('tests example', () => {
+  test('should pass when result match', () => {
+    expect(2 + 2).toEqual(4);
+  });
+});
